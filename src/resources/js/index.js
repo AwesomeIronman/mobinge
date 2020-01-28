4265537d370e8a3d9c5d0bb592fee6042d989951
Cyrus Gracias <cyrusgracias4u@gmail.com>
2020-01-28 14:07:33 +0530

    ADD: Trending Popular Upcoming movies

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -9,7 +9,101 @@ $(document).ready(() => {
     getSearchData(searchText);
   });
 
+  // Add event to show trending list
+  $('#btn-trending').on('click', (event) => {
+    event.preventDefault();
+
+    if ($("#root-div").hasClass("align-root-div")) {
+      console.log('Hiding top list');
+
+      $("#root-div").removeClass("align-root-div");
+      $("#top-list").css("display", "none");
+
+
+    } else {
+      $.ajax({
+        url: "https://api.themoviedb.org/3/trending/movie/week?api_key=dbc3ca0ccc9427a3db20ed940f2e04aa",
+        type: "GET"
+      })
+        .then((response) => {
+          console.log('Trending response: ', response.results);
+
+          $("#root-div").addClass("align-root-div");
+          $("#top-list").css("display", "block");
+          populateTopList(response.results);
+        })
+        .catch((err) => {
+          console.log(err);
+          return false;
+        });
+    }
+
+  });
+
+  // Add event to show popular list
+  $('#btn-popular').on('click', (event) => {
+    event.preventDefault();
+
+    if ($("#root-div").hasClass("align-root-div")) {
+      console.log('Hiding top list');
+
+      $("#root-div").removeClass("align-root-div");
+      $("#top-list").css("display", "none");
+
+
+    } else {
+      $.ajax({
+        url: "https://api.themoviedb.org/3/movie/popular?api_key=dbc3ca0ccc9427a3db20ed940f2e04aa&language=en-US&page=1&region=IN",
+        type: "GET"
+      })
+        .then((response) => {
+          console.log('Popular response: ', response.results);
+
+          $("#root-div").addClass("align-root-div");
+          $("#top-list").css("display", "block");
+          populateTopList(response.results);
+        })
+        .catch((err) => {
+          console.log(err);
+          return false;
+        });
+    }
+
+  });
+
+  // Add event to show upcoming list
+  $('#btn-upcoming').on('click', (event) => {
+    event.preventDefault();
+
+    if ($("#root-div").hasClass("align-root-div")) {
+      console.log('Hiding top list');
+
+      $("#root-div").removeClass("align-root-div");
+      $("#top-list").css("display", "none");
+
+
+    } else {
+      $.ajax({
+        url: "https://api.themoviedb.org/3/movie/upcoming?api_key=dbc3ca0ccc9427a3db20ed940f2e04aa&language=en-US&page=1&region=IN",
+        type: "GET"
+      })
+        .then((response) => {
+          console.log('Upcoming movies response: ', response.results);
+
+          $("#root-div").addClass("align-root-div");
+          $("#top-list").css("display", "block");
+          populateTopList(response.results);
+        })
+        .catch((err) => {
+          console.log(err);
+          return false;
+        });
+    }
+
+  });
+
 });
+// JQuery OnReady Close
 
 function getSearchData(searchText) {
 
@@ -29,7 +123,6 @@ function getSearchData(searchText) {
       console.log(err);
       return false;
     });
-
 }
 
 
@@ -38,12 +131,10 @@ function showSearchResult(response) {
   window.custom1 = movies // Variable declared in browser window for debugging
 
   if (movies.length > 0) {
-    let output = '';
-
     let sampleNode = $('#sampleNode')[0].cloneNode(true); // Create a clone to edit and append each time
     sampleNode.removeAttribute("id")
     sampleNode.removeAttribute("style")
-    
+
     $('#partial-info .list-group')[0].innerHTML = ""; // Remove old search result
 
     // Show basic information of each search result
@@ -51,7 +142,7 @@ function showSearchResult(response) {
       if (movie.media_type !== "person") { // Show info only if result is not of a person/actor
 
         console.log('title: ', movie.title, 'path: ', movie.poster_path);
-        
+
         sampleNode.querySelector(".movie-poster img").src = movie.poster_path ? `https://image.tmdb.org/t/p/w300${movie.poster_path}` : "#";
         sampleNode.querySelector(".movie-title").textContent = movie.title ? movie.title : movie.name;
         sampleNode.querySelector(".movie-release").textContent = movie.release_date ? movie.release_date : "UNSET";
@@ -103,4 +194,26 @@ function showTitleInfo(response) {
   $("#search-box").css("display", "none"); // Do not display search box
   $("#search-result").css("display", "none"); // Do not display search result div
   $("#search-info").css("display", "block"); // Display selected titles information
+}
+
+function populateTopList(data) {
+  $("#top-list .list-group").empty();
+
+  let sampleNode = $('#sampleNode')[0].cloneNode(true); // Create a clone to edit and append each time
+  sampleNode.removeAttribute("id")
+  sampleNode.removeAttribute("style")
+
+  $.each(data, (index, movie) => {
+    if (movie.media_type !== "person") { // Show info only if result is not of a person/actor
+
+      sampleNode.querySelector(".movie-poster img").src = movie.poster_path ? `https://image.tmdb.org/t/p/w300${movie.poster_path}` : "#";
+      sampleNode.querySelector(".movie-title").textContent = movie.title ? movie.title : movie.name;
+      sampleNode.querySelector(".movie-release").textContent = movie.release_date ? movie.release_date : "UNSET";
+      sampleNode.querySelector(".movie-rating").textContent = movie.vote_average ? movie.vote_average : "UNSET";
+
+      $("#top-list .list-group")[0].innerHTML += sampleNode.outerHTML; // Append edited sample node
+    }
+  });
+
+  $("#top-list").css("display", "block"); // Display search results
 }
