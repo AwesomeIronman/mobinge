b8b81b5b9383cda6b525a06653a46dda725f0016
Cyrus Gracias <cyrusgracias4u@gmail.com>
2020-03-15 10:38:23 +0530

    Favourites button alignment and layout

---
+++ src/resources/js/main.js
@@ -0,0 +1,6 @@
+// This prevents the page from scrolling down to where it was previously.
+if ('scrollRestoration' in history) {
+   history.scrollRestoration = 'manual';
+}
+// This is needed if the user scrolls down during page load and you want to make sure the page is scrolled to the top once it's fully loaded.
+window.scrollTo(0, 0);
